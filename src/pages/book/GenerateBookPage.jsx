da7456ba5e9399d4695848d036a19f50dab37a8e
JIHU96 <korjihu@gmail.com>
2023-03-18 23:25:25 +0900

    feat: 단어장 생성기 페이지 기능 구현

--- src/pages/book/GenerateBookPage.jsx
+++ src/pages/book/GenerateBookPage.jsx
@@ -1,9 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import arrowBackImg from '@/assets/svg/icons/icon-arrow-back-button.svg';
+import { instance } from '@/instance';
+import { useNavigate } from 'react-router-dom';
+
+const generateBook = async (bookName, sentense, navigate) => {
+  try {
+    const response = await instance.post('/book/generate', {
+      name: bookName,
+      text: sentense,
+    });
+    navigate(-1);
+    alert(response.date.comment);
+  } catch (e) {
+    //console.log(e);
+    alert('Error');
+  }
+};
 
 const GenerateBookPage = () => {
   const [textLength, setTextLength] = useState(0);
+  const [bookName, setBookName] = useState('');
+  const [sentense, setSentense] = useState('');
+  const navigate = useNavigate();
 
   return (
     <MainWrapper>
@@ -13,6 +32,19 @@ const GenerateBookPage = () => {
         </BackButton>
         <HeaderText>단어장 생성기</HeaderText>
       </BookHeader>
+
+      <BookNameWrapper>
+        <BookNameTitle>단어장 이름</BookNameTitle>
+        <BookNamingWrapper>
+          <BookNaming
+            type="text"
+            placeholder="단어장 이름"
+            onChange={e => {
+              setBookName(e.target.value);
+            }}
+          />
+        </BookNamingWrapper>
+      </BookNameWrapper>
 
       <GenerateWrapper>
         <GuideText>현재 단어장 생성기는 영어 to 한국어만 지원합니다.</GuideText>
@@ -25,6 +57,7 @@ const GenerateBookPage = () => {
         <SentenceInputWrapper>
           <SentenceInput
             onChange={e => {
+              setSentense(e.target.value);
               setTextLength(e.target.value.length);
             }}
             placeholder="영어 문장을 입력해주세요"
@@ -36,7 +69,7 @@ const GenerateBookPage = () => {
 
       <GenerateButton
         onClick={() => {
-          console.log(textLength);
+          generateBook(bookName, sentense, navigate);
         }}
       >
         생성하기
@@ -75,12 +108,36 @@ const BackButton = styled.button`
   height: 36px;
 `;
 const HeaderText = styled.div`
-  width: 146px;
-  height: 24px;
   font-weight: 500;
   font-size: 24px;
   line-height: 24px;
   color: #444444;
+`;
+//-- 단어장 이름 영역 --//
+const BookNameWrapper = styled.div`
+  width: 330px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+const BookNameTitle = styled.div`
+  font-weight: 500;
+  font-size: 24px;
+  color: #444444;
+`;
+const BookNamingWrapper = styled.div`
+  height: 48px;
+  background: #ffffff;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 11px;
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+const BookNaming = styled.input`
+  position: absolute;
+  left: 14px;
+  font-size: 16px;
 `;
 //-- 단어장 생성기 영역 --//
 const GenerateWrapper = styled.div`
@@ -91,7 +148,6 @@ const GenerateWrapper = styled.div`
   gap: 15px;
 `;
 const GuideText = styled.div`
-  width: 268px;
   height: 12px;
   font-weight: 400;
   font-size: 12px;
@@ -139,10 +195,11 @@ const SentenceInputWrapper = styled.div`
 const SentenceInput = styled.textarea`
   width: 302px;
   height: 196px;
-  font-weight: 400;
+  font-weight: 500;
   font-size: 12px;
   line-height: 12px;
   color: black;
+  outline: none;
   ::placeholder {
     color: #c0c0c0;
   }
