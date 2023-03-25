b0bd7adcb6557fddd82407e2df91a4a9d562eb51
JIHU96 <korjihu@gmail.com>
2023-03-26 00:31:23 +0900

    feat: word API 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import { authAPI } from './auth';
 import { userAPI } from './user';
+import { wordAPI } from './word';
 
 export const api = {
   auth: authAPI,
   user: userAPI,
+  word: wordAPI,
 };
