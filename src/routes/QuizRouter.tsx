34a1afdbcf1529973c54cdf3b83f82f2e44ac441
JIHU96 <korjihu@gmail.com>
2023-04-05 16:04:00 +0900

    feat: 단어암기, 단어퀴즈 페이지들 라우팅

--- src/routes/QuizRouter.tsx
+++ src/routes/QuizRouter.tsx
@@ -1,7 +1,12 @@
-import ChoicePage from '@/pages/quiz/ChoicePage';
-import FlashCardPage from '@/pages/quiz/FlashCardPage';
 import QuizPage from '@/pages/quiz/QuizPage';
-import ShortAnswerPage from '@/pages/quiz/ShortAnswerPage';
+import FlashCardPage from '@/pages/quiz/flashcard/FlashCardPage';
+import FlashCardWordPage from '@/pages/quiz/flashcard/FlashCardWordPage';
+import FlashCardMeanPage from '@/pages/quiz/flashcard/FlashCardMeanPage';
+import ChoicePage from '@/pages/quiz/choice/ChoicePage';
+import ChoiceQuestionPage from '@/pages/quiz/choice/ChoiceQuestionPage';
+import ShortAnswerPage from '@/pages/quiz/shortanswer/ShortAnswerPage';
+import ShortQuestionPage from '@/pages/quiz/shortanswer/ShortQuestionPage';
+import ResultPage from '@/pages/quiz/ResultPage';
 import { Route, Routes } from 'react-router-dom';
 
 const QuizRouter = () => {
@@ -10,14 +15,25 @@ const QuizRouter = () => {
       {/* 퀴즈 메인 페이지 */}
       <Route path={'/'} element={<QuizPage />} />
 
-      {/* 플래쉬카드 페이지 */}
+      {/* 플래쉬카드 메인 페이지 */}
       <Route path={'/flashcard'} element={<FlashCardPage />} />
+      {/* 플래쉬카드 영어암기 페이지 */}
+      <Route path={'/flashcard/word'} element={<FlashCardWordPage />} />
+      {/* 플래쉬카드 한글암기 페이지 */}
+      <Route path={'/flashcard/mean'} element={<FlashCardMeanPage />} />
 
-      {/* 객관식 페이지 */}
+      {/* 객관식 메인 페이지 */}
       <Route path={'/choice'} element={<ChoicePage />} />
+      {/* 객관식 문제 페이지 */}
+      <Route path={'/choice/question'} element={<ChoiceQuestionPage />} />
 
-      {/* 주관식 페이지 */}
+      {/* 주관식 메인 페이지 */}
       <Route path={'/shortanswer'} element={<ShortAnswerPage />} />
+      {/* 주관식 문제 페이지 */}
+      <Route path={'/shortanswer/question'} element={<ShortQuestionPage />} />
+
+      {/* 결과 페이지 */}
+      <Route path={'/result'} element={<ResultPage />} />
     </Routes>
   );
 };
