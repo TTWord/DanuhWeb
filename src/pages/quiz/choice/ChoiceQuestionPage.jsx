e0f4ddd6d43850d00cbbc5cce3e083504395b049
JIHU96 <korjihu@gmail.com>
2023-04-12 20:00:32 +0900

    feat: 객관식 퀴즈 기능 구현

--- src/pages/quiz/choice/ChoiceQuestionPage.jsx
+++ src/pages/quiz/choice/ChoiceQuestionPage.jsx
@@ -1,15 +1,72 @@
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { globalState } from '@/recoil';
 import xButton from '@/assets/svg/icons/icon-x-button.svg';
 
 const ChoiceQuestionPage = () => {
   const navigate = useNavigate();
 
+  const problems = [...useRecoilValue(globalState.quiz.quizList)];
+  // 현재 문제 목록
+  const [currentQuiz, setCurrentQuiz] = useState([]);
+  // 정답 번호
+  const [answerIndex, setAnswerIndex] = useState('');
+  // 맞출 문제
+  const [answer, SetAnswer] = useState([]);
+
   const goChoice = () => {
     navigate('/quiz/choice');
   };
 
-  const word = 'only';
+  // 맞춘 정답수
+  const [result, setResult] = useState(0);
+  // 문제 길이
+  const length = problems.length;
+  // 문제 진행도
+  const [number, setNumber] = useState(length !== 0 ? 1 : 0);
+
+  // 문제 가져오기
+  const extractQuiz = (value, number) => {
+    const result = value[number];
+
+    if (result) {
+      setCurrentQuiz(result.answers);
+      setAnswerIndex(result.answer_index);
+      SetAnswer(result.answers[result.answer_index]);
+    }
+  };
+
+  useEffect(() => {
+    // 받아온 퀴즈의 가장 앞 부분 추출, 요소 검사
+    extractQuiz(problems, 0);
+  }, []);
+
+  // 버튼에서 사용할 함수
+  const selectAnswer = select => {
+    if (number !== 10) {
+      if (select === answer[1]) {
+        setResult(current => current + 1);
+      }
+      setNumber(number + 1);
+      extractQuiz(problems, number);
+    } else {
+      navigate('/quiz/result', { state: { result: (result / length) * 100 } });
+    }
+  };
+
+  const CreateChoiceButtons = props => {
+    return (
+      <ChoiceButton
+        onClick={() => {
+          selectAnswer(props.example);
+        }}
+      >
+        {props.example}
+      </ChoiceButton>
+    );
+  };
 
   return (
     <MainWrapper>
@@ -21,22 +78,21 @@ const ChoiceQuestionPage = () => {
       </Header>
 
       <Content>
-        <ProgressBar value={20} max={100} />
+        <ProgressBar value={number * (100 / length)} max={100} />
 
         <ProgressWrapper>
           <ProgressIndex>
-            {1}/{10}
+            {number}/{length}
           </ProgressIndex>
           <ProgressTime value={20} max={100} />
         </ProgressWrapper>
 
-        <Word>{word}</Word>
+        <Word>{answer[0]}</Word>
 
         <ChoiceWrapper>
-          <ChoiceButton>{1}</ChoiceButton>
-          <ChoiceButton>{2}</ChoiceButton>
-          <ChoiceButton>{3}</ChoiceButton>
-          <ChoiceButton>{4}</ChoiceButton>
+          {currentQuiz.map((items, index) => {
+            return <CreateChoiceButtons key={index} example={items[1]} />;
+          })}
         </ChoiceWrapper>
       </Content>
     </MainWrapper>
