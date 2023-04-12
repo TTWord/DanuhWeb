8d4fb464d6e96b81884c34d03fcdd36caa4d8445
reality023 <reality023@naver.com>
2023-04-12 19:41:36 +0900

    fix: DarkMode Recoil 오류 FIX

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Router from '@/routes/Router';
-import { RecoilRoot, atom, useRecoilState, useRecoilValue } from 'recoil';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import { lightTheme } from './styles/theme';
+import { atom, useRecoilValue } from 'recoil';
 
 export const darkMode = atom({
   key: 'darkMode',
@@ -13,12 +13,10 @@ const App = () => {
   const isDark = useRecoilValue(darkMode);
 
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={isDark ? lightTheme : lightTheme}>
-        <GlobalStyles />
-        <Router />
-      </ThemeProvider>
-    </RecoilRoot>
+    <ThemeProvider theme={isDark ? lightTheme : lightTheme}>
+      <GlobalStyles />
+      <Router />
+    </ThemeProvider>
   );
 };
 
