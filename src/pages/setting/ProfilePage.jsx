62e6936380539d7dba382ef304383e07670fa18b
JIHU96 <korjihu@gmail.com>
2023-04-12 14:25:06 +0900

    fix: 프로필 변경 후 설정페이지로 돌아가게 수정

--- src/pages/setting/ProfilePage.jsx
+++ src/pages/setting/ProfilePage.jsx
@@ -125,6 +125,7 @@ const ProfilePage = () => {
           if (newNickname !== nickname && newNickname !== '') {
             changeNicknameAPI(newNickname);
           }
+          navigate('/setting');
         }}
       >
         <ApplyButton>적용하기</ApplyButton>
