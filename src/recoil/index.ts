8c4b68a1e1b459453f7a31f6d7097b7ecdf3bca4
JIHU96 <korjihu@gmail.com>
2023-04-12 13:35:06 +0900

    chore: 퀴즈 recoil 변수 추가

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,8 +1,11 @@
 import { layout } from './layout';
 import { auth } from './auth';
 import { setting } from './setting';
+import { quiz } from './quiz';
+
 export const globalState = {
   layout,
   auth,
   setting,
+  quiz,
 };
