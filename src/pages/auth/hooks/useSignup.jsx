033985697347c5c2c6488bdfad3b51b5cf072d88
JIHU96 <korjihu@gmail.com>
2023-05-11 20:00:47 +0900

    fix: JWT 토큰 저장 추가 & 에러 메세지 한글화

--- src/pages/auth/hooks/useSignup.jsx
+++ src/pages/auth/hooks/useSignup.jsx
@@ -12,13 +12,26 @@ const useSignup = () => {
         nickname,
         authCode,
       );
+      localStorage.setItem('access_Token', response.data.access_token);
+      localStorage.setItem('refresh_Token', response.data.refresh_token);
       navigate('/auth/join/welcome');
     } catch (e) {
       const errorMessage = e.response.data.message;
-      Swal.fire({
-        icon: 'error',
-        title: errorMessage,
-      });
+      switch (errorMessage) {
+        case 'EXPIRED_AUTH_CODE':
+          Swal.fire({
+            icon: 'error',
+            title: '인증코드가 만료되었습니다.',
+          });
+          break;
+
+        case 'INCORRECT_AUTH_CODE':
+          Swal.fire({
+            icon: 'error',
+            title: '인증코드가 올바르지 않습니다.',
+          });
+          break;
+      }
     }
   };
 
