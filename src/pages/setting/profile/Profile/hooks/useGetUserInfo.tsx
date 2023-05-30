f5a13e00e924a366910e22ae6982865c5b78863f
JIHU96 <korjihu@gmail.com>
2023-05-30 17:34:23 +0900

    feat: 회원정보 가져오는 hook 추가

---
+++ src/pages/setting/profile/Profile/hooks/useGetUserInfo.tsx
@@ -0,0 +1,26 @@
+import { api } from '@/api';
+import { AxiosError } from 'axios';
+import Swal from 'sweetalert2';
+
+const useGetUserInfo = () => {
+  const getUserInfo = async () => {
+    try {
+      const { data: response } = await api.user.getUserInfo();
+
+      return response;
+    } catch (e: unknown) {
+      const err = e as AxiosError<{
+        message: string;
+      }>;
+      const errorMessage = err?.response?.data.message;
+      Swal.fire({
+        icon: 'error',
+        title: errorMessage,
+      });
+    }
+  };
+
+  return getUserInfo;
+};
+
+export default useGetUserInfo;
