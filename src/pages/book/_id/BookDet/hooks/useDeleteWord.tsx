52b8d013b1d9d0e65c1d61702d8ca3d3455e3e38
JIHU96 <korjihu@gmail.com>
2023-05-31 18:44:29 +0900

    fix: tsx로 변환

--- src/pages/book/_id/BookDet/hooks/useDeleteWord.tsx
+++ src/pages/book/_id/BookDet/hooks/useDeleteWord.tsx
@@ -1,14 +1,18 @@
 import { api } from '@/api';
 import Swal from 'sweetalert2';
+import { AxiosError } from 'axios';
 
 const useDeleteWord = () => {
-  const deleteWord = async wordId => {
+  const deleteWord = async (wordId: number) => {
     try {
       const { data: response } = await api.word.deleteWord(wordId);
       return response;
-    } catch (e) {
-      const errorCode = e.response.status;
-      const errorMessage = e.response.data.message;
+    } catch (e: unknown) {
+      const err = e as AxiosError<{
+        message: string;
+      }>;
+      const errorCode = err?.response?.status;
+      const errorMessage = err?.response?.data.message;
 
       if (errorCode === 403 || errorCode === 404) {
         Swal.fire({
