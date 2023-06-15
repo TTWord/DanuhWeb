7ebae1f63535156cc0ca0639905c086a461ddc15
JIHU96 <korjihu@gmail.com>
2023-06-15 19:40:58 +0900

    chore: 객관식 퀴즈 페이지, 불필요한 console 출력 제거

--- src/pages/quiz/choice/ChoiceQuestion/ChoiceQuestionPage.tsx
+++ src/pages/quiz/choice/ChoiceQuestion/ChoiceQuestionPage.tsx
@@ -210,7 +210,6 @@ const ChoiceQuestionPage = () => {
       <ChoiceButton
         onClick={() => {
           selectAnswer(props.example);
-          console.log('버튼 내부');
           setIsAnswered(current => !current);
         }}
       >
