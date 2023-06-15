c2e1811e6cc7af82c1308c62997e73cefcdcebba
JIHU96 <korjihu@gmail.com>
2023-06-15 19:00:13 +0900

    feat: 단어 암기 상태 변경 API 추가

--- src/api/memo.ts
+++ src/api/memo.ts
@@ -5,6 +5,11 @@ interface getMemorizeWordParams {
   count: number;
 }
 
+interface patchMemorizedStatus {
+  wordId: number;
+  isMemorized: string | boolean;
+}
+
 export const memoAPI = {
   getMemorizeWord: async ({ bookId, count }: getMemorizeWordParams) => {
     const response = await instance.get(`/memo`, {
@@ -12,6 +17,18 @@ export const memoAPI = {
         book_ids: bookId,
         count: count,
       },
+    });
+
+    return response;
+  },
+
+  patchMemorizedStatus: async ({
+    wordId,
+    isMemorized,
+  }: patchMemorizedStatus) => {
+    const response = await instance.patch(`/memo`, {
+      word_id: wordId,
+      is_memorized: isMemorized,
     });
 
     return response;
