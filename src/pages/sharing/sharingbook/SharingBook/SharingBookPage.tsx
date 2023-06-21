b1e162ff20dc4965ae63e7da389e1a6542934c19
JIHU96 <korjihu@gmail.com>
2023-06-21 20:29:42 +0900

    feat: 공유 페이지에서 필요한 페이지들 추가

---
+++ src/pages/sharing/sharingbook/SharingBook/SharingBookPage.tsx
@@ -0,0 +1,9 @@
+const SharingBookPage = () => {
+  return (
+    <div>
+      <div>1</div>
+      <div>2</div>
+    </div>
+  );
+};
+export default SharingBookPage;
--- src/routes/SharingRouter.tsx
+++ src/routes/SharingRouter.tsx
@@ -3,6 +3,9 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import RouteTransition from './RouteTransition';
 import RouteTransitionWrapper from './RouteTransitionWrapper';
 import RootLayout from '@/components/layout/RootLayout';
+import MySharingPage from '@/pages/sharing/mysharing/MySharing/MySharingPage';
+import SearchBooksPage from '@/pages/sharing/search/SearchBooks/SearchBooksPage';
+import SharingBookPage from '@/pages/sharing/sharingbook/SharingBook/SharingBookPage';
 
 const SharingRouter = () => {
   const location = useLocation();
@@ -11,11 +14,20 @@ const SharingRouter = () => {
     <RouteTransition location={location}>
       <Routes location={location}>
         <Route element={<RouteTransitionWrapper />}>
-          {/* 랭킹 메인 페이지 */}
+          {/* 공유 메인 페이지 */}
 
           <Route element={<RootLayout />}>
             <Route path={'/'} element={<SharingPage />} />
           </Route>
+
+          {/* 내 공유 단어장 목록 페이지 */}
+          <Route path={'/mysharing'} element={<MySharingPage />} />
+
+          {/* 검색 페이지 */}
+          <Route path={'/search'} element={<SearchBooksPage />} />
+
+          {/* 공유 단어장 페이지 */}
+          <Route path={'/sharingbook/:id'} element={<SharingBookPage />} />
         </Route>
       </Routes>
     </RouteTransition>
