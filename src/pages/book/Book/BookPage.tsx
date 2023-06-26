106a3ae7acb47393c599a086d56d6f4f6a69d3b6
JIHU96 <korjihu@gmail.com>
2023-06-26 21:39:41 +0900

    fix: 단어장페이지 디자인 수정

--- src/pages/book/Book/BookPage.tsx
+++ src/pages/book/Book/BookPage.tsx
@@ -2,6 +2,7 @@ import Additional from '@/pages/book/Book/components/Additional';
 import { styled } from 'twin.macro';
 import BookItem from '@/pages/book/Book/components/BookItem';
 import useBookPageLogic from './hooks/useBookPageLogic';
+import danuhLogo from '@/assets/svg/logos/logo-danuh-small.svg';
 
 const BookPage = () => {
   const { books, onItemClick, onClickUpdate, onClickRemove } =
@@ -10,33 +11,56 @@ const BookPage = () => {
   if (!books) return null;
 
   return (
-    <Container>
-      <Items>
-        {books.map((book: any) => (
-          <BookItem
-            key={book.id}
-            book={book}
-            onItemClick={onItemClick}
-            onClickUpdate={onClickUpdate}
-            onClickRemove={onClickRemove}
-          />
-        ))}
-      </Items>
-      <Additional />
-    </Container>
+    <WebWrapper>
+      <Header>
+        <img src={danuhLogo} alt="logo" />
+      </Header>
+
+      <Container>
+        <Items>
+          {books.map((book: any) => (
+            <BookItem
+              key={book.id}
+              book={book}
+              onItemClick={onItemClick}
+              onClickUpdate={onClickUpdate}
+              onClickRemove={onClickRemove}
+            />
+          ))}
+        </Items>
+        <Additional />
+      </Container>
+    </WebWrapper>
   );
 };
 
 export default BookPage;
 
+const WebWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  height: 52px;
+  background-color: ${({ theme }) => theme.colors.primary[200]};
+  padding-top: 14px;
+  padding-left: 8px;
+  z-index: -2;
+
+  img {
+    z-index: -1;
+  }
+`;
+
 const Container = styled.div`
-  position: relative;
+  width: 100%;
 `;
 
 const Items = styled.div`
   width: 100%;
-  padding: 20px 20px;
-  gap: 20px;
+  padding: 20px 16px;
   display: flex;
   flex-direction: column;
 `;
