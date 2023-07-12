f3a910628af66cf7d5f076f34fb438163230d629
JIHU96 <korjihu@gmail.com>
2023-07-12 16:28:26 +0900

    feat: 프로필 변경 페이지 디자인 수정

--- src/pages/setting/profile/Profile/ProfilePage.tsx
+++ src/pages/setting/profile/Profile/ProfilePage.tsx
@@ -5,8 +5,8 @@ import { useNavigate } from 'react-router-dom';
 import useGetUserInfo from '@/pages/setting//profile/Profile/hooks/useGetUserInfo';
 import useChangeNickname from '@/pages/setting//profile/Profile/hooks/useChangeNickname';
 import useChangeProfilePic from './hooks/useChangeProfilePic';
-import backImg from '@/assets/svg/icons/icon-back-button.svg';
-import uploadImg from '@/assets/svg/icons/icon-image-upload.svg';
+import backIcon from '@/assets/svg/icons/icon-back-gray.svg';
+import removeIcon from '@/assets/svg/icons/Icon-input-x-button.svg';
 import useNavigatePop from '@/hooks/useNavigatePop';
 
 const ProfilePage = () => {
@@ -14,13 +14,14 @@ const ProfilePage = () => {
   const getUserInfo = useGetUserInfo();
   const changeNewNickname = useChangeNickname();
 
-  const [nickname, setNickname] = useState<string>('');
-  const [username, setUsername] = useState<string>('');
-  const [profilePic, setProfilePic] = useState<string>('');
-  const [newNickname, setNewNickname] = useState<string>('');
+  const [nickname, setNickname] = useState('');
+  const [username, setUsername] = useState('');
+  const [profilePic, setProfilePic] = useState('');
+  const [newNickname, setNewNickname] = useState('');
 
   const changeProfilePic = useChangeProfilePic();
   const profilePicRef = useRef<any>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const [file, setFile] = useState<string | Blob>();
 
   const uploadImage = () => {
@@ -47,9 +48,14 @@ const ProfilePage = () => {
     navigatePop('/setting');
   };
 
+  const removeNickname = () => {
+    if (inputRef.current) inputRef.current.value = '';
+  };
+
   useEffect(() => {
     const setUserInfo = async () => {
       const { data: response } = await getUserInfo();
+
       setUsername(response.username);
       setNickname(response.nickname);
       setProfilePic(response.url);
@@ -63,15 +69,15 @@ const ProfilePage = () => {
 
   return (
     <MainWrapper>
-      <HeaderWrapper>
+      <Header>
         <BackButton onClick={goBack}>
-          <img src={backImg} alt="backImg" />
+          <img src={backIcon} alt="backImg" />
         </BackButton>
         프로필 변경
-      </HeaderWrapper>
+      </Header>
 
       <ContentWrapper>
-        <ImgWrapper>
+        <ImgWrapper onClick={uploadOnClick}>
           <ProfilePic src={profilePic} />
           <UploadImg
             type="file"
@@ -79,16 +85,17 @@ const ProfilePage = () => {
             onChange={uploadImage}
             accept=".jpg, .png, .jpeg"
           />
-          <button onClick={uploadOnClick}>
-            <img src={uploadImg} alt="uploadImg" />
-          </button>
         </ImgWrapper>
-        <Nickname
-          type="text"
-          placeholder={nickname}
-          onChange={changeNickname}
-        />
-        <Line />
+        <NicknameWrapper>
+          <Nickname
+            ref={inputRef}
+            type="text"
+            placeholder={nickname}
+            onChange={changeNickname}
+          />
+          <img onClick={removeNickname} src={removeIcon} alt="deleteIcon" />
+        </NicknameWrapper>
+
         <Username>{username}</Username>
       </ContentWrapper>
 
@@ -105,7 +112,7 @@ const ProfilePage = () => {
           }
         }}
       >
-        <ApplyButton>적용하기</ApplyButton>
+        <ApplyButton>저장하기</ApplyButton>
       </ProfileForm>
     </MainWrapper>
   );
@@ -119,100 +126,109 @@ const MainWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  font-style: normal;
 `;
 
-const HeaderWrapper = styled.div`
-  position: relative;
+const Header = styled.div`
   width: 100%;
-  height: 62px;
-  border-bottom: 1px solid #666666;
-  font-weight: 500;
+  height: 56px;
+  font-weight: 600;
   font-size: 16px;
+  font-weight: 600;
+  line-height: 140%;
   display: flex;
-  justify-content: center;
   align-items: center;
+  padding: 0 16px;
+  flex-shrink: 0;
 `;
+
 const BackButton = styled.button`
-  position: absolute;
-  left: 20px;
-  img {
-    height: 12px;
-  }
+  margin-right: 16px;
 `;
 
 const ContentWrapper = styled.div`
   width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 50px;
+  padding: 24px 56px;
 `;
+
 const ImgWrapper = styled.div`
-  position: relative;
-  width: 216px;
-  height: 216px;
-  background: #e0e0e0;
-  border-radius: 20px;
-  margin-bottom: 17px;
+  width: 140px;
+  height: 140px;
+  aspect-ratio: 1/1;
+  background-color: ${({ theme }) => theme.colors.gray[400]};
+  border-radius: 100%;
+  margin-bottom: 32px;
   display: flex;
   align-items: center;
   box-sizing: border-box;
   border: 1px solid #e0e0e0;
-  button {
-    position: absolute;
-    right: 20px;
-    bottom: 22px;
+  border-radius: 140%;
+
+  :hover {
+    cursor: pointer;
   }
 `;
+
 const UploadImg = styled.input`
   display: none;
 `;
 
 const ProfilePic = styled.img`
-  width: 216px;
-  height: 216px;
-  border-radius: 20px;
+  width: 140px;
+  height: 140px;
+  aspect-ratio: 1/1;
+  border-radius: 140%;
+`;
+
+const NicknameWrapper = styled.div`
+  width: 100%;
+  padding: 10px 16px;
+  color: #000000;
+  border-bottom: 2px solid ${({ theme }) => theme.colors.primary[600]};
+  margin-bottom: 12px;
+  display: flex;
 `;
 
 const Nickname = styled.input`
-  width: 216px;
-  font-weight: 300;
-  font-size: 20px;
-  line-height: 20px;
-  text-align: center;
-  color: #000000;
+  width: 100%;
+  height: 100%;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 140%;
+  outline: none;
+
   ::placeholder {
-    color: #e0e0e0;
+    color: ${({ theme }) => theme.colors.gray[400]};
   }
 `;
-const Line = styled.div`
-  width: 216px;
-  border: 1px solid #cfcfcf;
-  margin: 6px 0 6px 0;
-`;
+
 const Username = styled.div`
   font-weight: 300;
   font-size: 12px;
   line-height: 12px;
-  color: #333333;
-`;
-
-const ApplyButton = styled.button`
-  position: fixed;
-  bottom: 25px;
-  width: 344px;
-  height: 80px;
-  background: #724fab;
-  border-radius: 5px;
-  font-weight: 300;
-  font-size: 20px;
-  line-height: 20px;
-  text-align: center;
-  color: #ffffff;
+  color: ${({ theme }) => theme.colors.gray[400]};
 `;
 
 const ProfileForm = styled.form`
   width: 100%;
   display: flex;
   justify-content: center;
+  align-items: center;
+  padding: 24px 20px;
+`;
+
+const ApplyButton = styled.button`
+  width: 100%;
+  height: 48px;
+  background-color: ${({ theme }) => theme.colors.primary.default};
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 140%;
+  text-align: center;
+  color: #ffffff;
 `;
