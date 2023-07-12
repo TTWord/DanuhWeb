3b083d57d57b16a6fd8e3b538a3893474c12c86e
JIHU96 <korjihu@gmail.com>
2023-07-12 19:25:37 +0900

    feat: 단어장에 단어가 0개일때의 컴포넌트 추가

--- src/pages/book/_id/BookDet/BookDetPage.tsx
+++ src/pages/book/_id/BookDet/BookDetPage.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { useNavigate, useParams } from 'react-router-dom';
-import sharingIcon from '@/assets/svg/icons/icon-share.svg';
-import plusIcon from '@/assets/svg/icons/icon-plus.svg';
+import { useParams } from 'react-router-dom';
 import NewWord from './components/NewWord';
 import useGetWord from './hooks/useGetWord';
 import useGetBookById from '@/pages/book/_id/hooks/useGetBookById';
@@ -10,12 +8,12 @@ import useDeleteWord from '@/pages/book/_id/BookDet/hooks/useDeleteWord';
 import StackLayout from '@/components/layout/StackLayout';
 import DetPlus from './components/DetPlus';
 import DetShare from './components/DetShare';
+import EmptyWord from './components/EmptyWord';
 
 const BookDetPage = () => {
   const getWord = useGetWord();
   const getBookById = useGetBookById();
   const deleteWord = useDeleteWord();
-  const navigate = useNavigate();
 
   const bookId: number = Number(useParams().id);
   const [bookName, setBookName] = useState('');
@@ -24,6 +22,7 @@ const BookDetPage = () => {
 
   const getBook = async () => {
     const response = await getWord(bookId);
+
     setWords(response);
   };
 
@@ -60,6 +59,8 @@ const BookDetPage = () => {
       }}
     >
       <BookContainer>
+        {words.length === 0 && <EmptyWord bookId={bookId} />}
+
         {words.map(items => {
           return (
             <NewWord
@@ -85,8 +86,6 @@ const BookDetPage = () => {
 
 export default BookDetPage;
 
-//== 스타일 정의 ==//
-//-- 단어 --//
 const BookContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -96,60 +95,9 @@ const BookContainer = styled.div`
   padding-bottom: 40px;
   flex-direction: column;
   align-items: center;
-  overflow: auto;
+  overflow: scroll;
+
   ::-webkit-scrollbar {
     display: none;
-  }
-`;
-
-//-- 하단 아이콘 --//
-// fixed로 하여 수정 예정
-const BookFooter = styled.div`
-  width: 100%;
-  height: 85px; //
-  display: flex;
-  justify-content: end;
-  background-color: transparent;
-`;
-
-const IconWrapper = styled.div`
-  position: fixed;
-  display: flex;
-  justify-content: end;
-  background-color: transparent;
-`;
-
-const Circle = styled.button`
-  width: 58px;
-  height: 58px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  & + & {
-    //margin-left: 15px;
-  }
-`;
-
-const SharingButton = styled(Circle)`
-  position: fixed;
-  background-color: #8ee9f6;
-  right: 99px;
-  bottom: 27px;
-`;
-
-const PlusButton = styled(Circle)`
-  position: fixed;
-  right: 26px;
-  bottom: 27px;
-  justify-content: center;
-  background: linear-gradient(180deg, #703ab6 0%, #774178 100%);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-
-  img {
-    width: 36px;
-    height: 36px;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
   }
 `;
