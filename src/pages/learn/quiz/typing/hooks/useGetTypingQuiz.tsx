3745dc724a1f5deeae85627f38661d79ee4bb516
JIHU96 <korjihu@gmail.com>
2023-07-17 23:12:01 +0900

    fix: 주관식 퀴즈 페이지 tsx로 변경 & 퀴즈 API 훅 생성

---
+++ src/pages/learn/quiz/typing/hooks/useGetTypingQuiz.tsx
@@ -0,0 +1,48 @@
+import { api } from '@/api';
+import { useNavigate } from 'react-router-dom';
+import { AxiosError } from 'axios';
+import useToast from '@/hooks/useToast';
+
+interface getQuizParams {
+  bookIds: number[];
+  count: number;
+  memorizedFilter: boolean;
+}
+
+const useGetTypingQuiz = () => {
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const getTypingQuiz = async ({
+    bookIds,
+    count,
+    memorizedFilter,
+  }: getQuizParams) => {
+    try {
+      const { data: response } = await api.quiz.getTypingQuiz({
+        bookIds,
+        count,
+        memorizedFilter,
+      });
+
+      return response;
+    } catch (e) {
+      const err = e as AxiosError<{
+        message: string;
+      }>;
+      const errorMessage = err.response?.data?.message;
+
+      if (errorMessage === 'WORD_LESS_THAN_COUNT') {
+        toast.error('단어 개수가 4개 이상 필요합니다.');
+      } else {
+        toast.error(errorMessage || '오류가 발생했습니다.');
+      }
+
+      navigate('/learn/quiz');
+    }
+  };
+
+  return getTypingQuiz;
+};
+
+export default useGetTypingQuiz;
