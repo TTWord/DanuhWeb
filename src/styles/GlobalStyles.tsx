1f8264451dc40fa02b64e70d668becfc8ca1f218
JIHU96 <korjihu@gmail.com>
2023-08-05 21:03:59 +0900

    feat: 전역 textarea에 스크롤바 숨김 처리

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -73,6 +73,10 @@ const CustomStyles = createGlobalStyle`
     display: none;
   }
 
+  textarea::-webkit-scrollbar {
+    display: none;
+  }
+
   input:-webkit-autofill {
    -webkit-box-shadow: 0 0 0 1000px #ffffff inset;
    -webkit-text-size-adjust: inherit;
