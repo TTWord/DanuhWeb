413d97758c7665d12e81af7e1b5236947b111da2
JIHU96 <korjihu@gmail.com>
2023-08-06 00:44:56 +0900

    feat: 공유 단어장 다운로드 API에 에러 분기점 추가

--- src/pages/share/sharebook/ShareBook/hooks/useDownloadSharedBook.tsx
+++ src/pages/share/sharebook/ShareBook/hooks/useDownloadSharedBook.tsx
@@ -1,8 +1,5 @@
 import { api } from '@/api';
 import { AxiosError } from 'axios';
-import Swal from 'sweetalert2';
-import { useSetRecoilState } from 'recoil';
-import { toastStatus } from '@/components/common/toast/Toast';
 import { useNavigate } from 'react-router-dom';
 import useToast from '@/hooks/useToast';
 
@@ -25,11 +22,23 @@ const useDownloadSharedBook = () => {
       const errorMessage = err?.response?.data.message;
 
       switch (errorMessage) {
+        case 'SHARE_NOT_FOUND': {
+          toast.error('공유 단어장이 존재하지 않습니다.');
+          return;
+        }
         case 'SHARE_BOOK_OWNER': {
           toast.error('소유중인 단어장은 다운받을 수 없습니다.');
           return;
         }
-
+        case 'SHARE_ALREADY_EXIST': {
+          toast.error('이미 다운로드한 단어장입니다.');
+          return;
+        }
+        case 'BOOK_NOT_SHARED': {
+          // 버그인지 아닌지는 아직 모르는 부분
+          toast.error('공유중인 단어장이 아닙니다.');
+          return;
+        }
         default: {
           toast.error('에러가 발생하였습니다.');
           return;
