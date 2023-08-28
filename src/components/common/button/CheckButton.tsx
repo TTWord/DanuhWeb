0eeed33732f5dff7dc921899685d3126e01f5fb3
JIHU96 <korjihu@gmail.com>
2023-08-28 22:31:59 +0900

    fix: 중복확인 버튼 크기 수정

--- src/components/common/button/CheckButton.tsx
+++ src/components/common/button/CheckButton.tsx
@@ -1,7 +1,7 @@
 import styled, { css } from 'styled-components';
 
 interface ButtonProps {
-  onClick?: () => void;
+  onClick: () => void;
 }
 
 const CheckButton: React.FC<ButtonProps> = ({ onClick }) => {
@@ -17,12 +17,8 @@ const Button = styled.button`
   justify-content: center;
   align-items: center;
   flex-shrink: 0;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 140%;
   color: white;
   background-color: ${({ theme }) => theme.colors.primary[800]};
   border-radius: 8px;
-  font-family: ${({ theme }) => theme.fonts.gmarketSans};
+  ${({ theme }) => theme.typography.gmarketSans.md[12]};
 `;
