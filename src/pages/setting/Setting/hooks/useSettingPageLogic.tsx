a169dc5d88c4271256613f2f6c19c5ef451af7bd
JIHU96 <korjihu@gmail.com>
2023-09-22 16:11:02 +0900

    refactor: 설정페이지의 useEffect 부분을 로직훅으로 이동

--- src/pages/setting/Setting/hooks/useSettingPageLogic.tsx
+++ src/pages/setting/Setting/hooks/useSettingPageLogic.tsx
@@ -1,8 +1,17 @@
 import { api } from '@/api';
 import useNavigatePush from '@/hooks/useNavigatePush';
+import { globalState } from '@/recoil';
+import { useSetRecoilState } from 'recoil';
+import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 
 const useSettingPageLogic = () => {
+  const setActiveMenu = useSetRecoilState(globalState.layout.activeMenuNumber);
+
+  useEffect(() => {
+    setActiveMenu(3);
+  }, []);
+
   const navigatePush = useNavigatePush();
   const { data: about } = useQuery('MyPage/GetMyAbout', async () => {
     const { data: response } = await api.user.getMyInfo();
