09c0138d44a45df0a9246632413cef6b1b892af5
JIHU96 <korjihu@gmail.com>
2024-01-10 15:17:14 +0900

    feat: 블라인드 암기/퀴즈 추가

--- src/pages/learn/Learn/data/LearnData.tsx
+++ src/pages/learn/Learn/data/LearnData.tsx
@@ -1,12 +1,22 @@
 import picFlashCard from './images/flashcard.png';
 import picSelect from './images/select.png';
 import picTyping from './images/typing.png';
+import picBlindMemo from './images/blind-memo.png';
+import picBlindQuiz from './images/blind-quiz.png';
+
+// 블라인드 추가하기
 
 export const memoList = [
   {
     title: '단어암기',
     type: 'flashcard',
     icon: picFlashCard,
+    iconWidth: '102px',
+  },
+  {
+    title: '단어암기',
+    type: 'blind',
+    icon: picBlindMemo,
     iconWidth: '102px',
   },
 ];
@@ -22,6 +32,20 @@ export const quizList = [
     title: '주관식',
     type: 'typing',
     icon: picTyping,
+    iconWidth: '102px',
+  },
+  {
+    title: '객관식',
+    type: 'blind',
+    typeDetail: 'choice',
+    icon: picBlindQuiz,
+    iconWidth: '102px',
+  },
+  {
+    title: '주관식',
+    type: 'blind',
+    typeDetail: 'typing',
+    icon: picBlindQuiz,
     iconWidth: '102px',
   },
 ];
